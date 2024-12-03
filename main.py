1bcc572281a6d2bcc2d455ae82ed38377290c2e1
The_cahar <102082606+thecahar@users.noreply.github.com>
2024-12-03 19:44:14 +0200

    main.py edited

--- main.py
+++ main.py
@@ -1,83 +1,166 @@
-COMMANDS = ("quit", "show", "retrieve", "add")
-
-students = [
-    {
+students = {
+    1: {
         "name": "John Doe",
         "marks": [4, 5, 1, 4, 5, 2, 5],
         "info": "John is 22 y.o. Hobbies: music",
     },
-    {
+    2: {
         "name": "Marry Black",
         "marks": [4, 1, 3, 4, 5, 1, 2, 2],
         "info": "John is 23 y.o. Hobbies: football",
     },
-]
+}
+
+LAST_ID_CONTEXT = 2
 
 
-def find_student(name: str) -> dict | None:
-    for student in students:
-        if student["name"] == name:
-            return student
+def represent_students():
+    for id_, student in students.items():
+        print(f"[{id_}] {student['name']}, marks: {student['marks']}")
 
-    return None
+def add_student(student: dict) -> dict | None:
+    global LAST_ID_CONTEXT
 
+    if len(student) != 2:
+        return None
+    elif not student.get("name") or not student.get("marks"):
+        return None
+    else:
+        LAST_ID_CONTEXT += 1
+        students[LAST_ID_CONTEXT] = student
 
-def show_students() -> None:
-    print("=" * 20)
-    print("The list of students:\n")
-    for student in students:
-        print(f"{student['name']}. Marks: {student['marks']}")
-
-    print("=" * 20)
-
-
-def show_student(name: str) -> None:
-    student: dict | None = find_student(name)
-
-    if not student:
-        print(f"There is no student {name}")
-        return
-
-    print("Detailed about student:\n")
-    print(
-        f"{student['name']}. Marks: {student['marks']}\n"
-        f"Details: {student['info']}\n"
-    )
+    return student
 
 
-def add_student(student_name: str):
-    instance = {"name": student_name, "marks": [], "info": None}
-    students.append(instance)
-
-    return instance
+def search_student(id_: int) -> dict | None:
+    return students.get(id_)
 
 
-def main():
-    print(f"Welcome to the Digital journal!\nAvailable commands: {COMMANDS}")
-    while True:
-        user_input = input("Enter the command: ")
+def delete_student(id_: int):
+    if search_student(id_):
+        del students[id_]
+        print(f"Student with id '{id_}' is deleted")
+    else:
+        print(f"There is student '{id_}' in the storage")
 
-        if user_input not in COMMANDS:
-            print(f"Command {user_input} is not available.\n")
-            continue
 
-        if user_input == "quit":
-            print("See you next time.")
-            break
+def update_student(id_: int, payload: dict) -> dict:
+    students[id_] = payload
+    return payload
+
+
+def student_details(student: dict) -> None:
+    print(f"Detailed info: [{student['name']}]...")
+
+def parse(data: str) -> tuple[str, list[int]]:
+    template = "John Doe;4,5,4,5,4,5"
+
+    items = data.split(";")
+
+    if len(items) != 2:
+        raise Exception(f"Incorrect data. Template: {template}")
+    name, raw_marks = items
+
+    try:
+        marks = [int(item) for item in raw_marks.split(",")]
+    except ValueError as error:
+        print(error)
+        raise Exception(f"Marks are incorrect. Template: {template}") from error
+
+    return name, marks
+
+
+def ask_student_payload():
+    prompt = "Enter student's payload using next template:\n'John Doe;4,5,4,5,4,5': "
+
+    if not (payload := parse(input(prompt))):
+        return None
+    else:
+        name, marks = payload
+
+    return {"name": name, "marks": marks}
+
+
+def handle_management_command(command: str):
+    if command == "show":
+        represent_students()
+
+    elif command == "retrieve":
+        search_id = input("Enter student's id to retrieve: ")
 
         try:
-            if user_input == "show":
-                show_students()
-            elif user_input == "retrieve":
-                student_name = input("Enter student name you are looking for: ")
-                show_student(student_name)
-            elif user_input == "add":
-                name = input("Enter student's name: ")
-                add_student(name)
-        except NotImplementedError as error:
-            print(f"Feature '{error}' is not ready for live.")
-        except Exception as error:
-            print(error)
+            id_ = int(search_id)
+        except ValueError as error:
+            raise Exception(f"ID '{search_id}' is not correct value") from error
+        else:
+            if student := search_student(id_):
+                student_details(student)
+            else:
+                print(f"There is not student with id: '{id_}'")
+
+    elif command == "remove":
+        delete_id = input("Enter student's id to remove: ")
+
+        try:
+            id_ = int(delete_id)
+        except ValueError as error:
+            raise Exception(f"ID '{delete_id}' is not correct value") from error
+        else:
+            delete_student(id_)
+
+    elif command == "change":
+        update_id = input("Enter student's id you wanna change: ")
+
+        try:
+            id_ = int(update_id)
+        except ValueError as error:
+            raise Exception(f"ID '{update_id}' is not correct value") from error
+        else:
+            if data := ask_student_payload():
+                update_student(id_, data)
+                print(f"Student is updated")
+                if student := search_student(id_):
+                    student_details(student)
+                else:
+                    print(f"Can not change user with data {data}")
+
+    elif command == "add":
+        data = ask_student_payload()
+        if data is None:
+            return None
+        else:
+            if not (student := add_student(data)):
+                print(f"Can't create user with data: {data}")
+            else:
+                print(f"New student '{student['name']}' is created")
+    else:
+        raise SystemExit(f"Unrecognized command: '{command}'")
 
 
-main()
+def handle_user_input():
+    SYSTEM_COMMANDS = ("quit", "help")
+    MANAGEMENT_COMMANDS = ("show", "add", "retrieve", "remove", "change")
+    AVAILABLE_COMMANDS = SYSTEM_COMMANDS + MANAGEMENT_COMMANDS
+
+    help_message = (
+        "Welcome to the Journal application. Use the menu to interact with the application.\n"
+        f"Available commands: {AVAILABLE_COMMANDS}"
+    )
+
+    print(help_message)
+
+    while True:
+        command = input("Enter the command: ")
+
+        if command == "quit":
+            print(f"\nThanks for using Journal application. Bye!")
+            break
+        elif command == "help":
+            print(help_message)
+        elif command in MANAGEMENT_COMMANDS:
+            handle_management_command(command=command)
+        else:
+            print(f"Unrecognized command '{command}'")
+
+
+handle_user_input()
